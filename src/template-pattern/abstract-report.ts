7bc1f3004bf067b2c416124af40fb90013811e59
Luc Joosten <120277042+lhajoosten@users.noreply.github.com>
2023-03-16 21:44:18 +0100

    updated template method

--- src/template-pattern/abstract-report.ts
+++ src/template-pattern/abstract-report.ts
@@ -2,18 +2,6 @@ import { User } from "../models/user.model";
 import { SprintResult } from "./SprintResult";
 
 export abstract class Report {
-  // public author: User;
-  // public timeOfGeneration: string;
-  // public format: string
-  // public sprintResult: ISprintResult
-
-  // public constructor(author: User, format: string, result: ISprintResult) {
-  //   this.author = author;
-  //   this.timeOfGeneration = new Date().getTime.toString();
-  //   this.format = format;
-  //   this.sprintResult = result
-  // }
-
   generateReport(): string {
     const header = this.generateHeader();
     const content = this.generateContent();
@@ -46,6 +34,49 @@ export abstract class Report {
   protected abstract generateContent(): string;
 }
 
+export function exportLog(sprintResult: SprintResult): string {
+  const sprintDuration = Math.round(
+    (new Date(sprintResult["endDate"]).getTime() -
+      new Date(sprintResult["startDate"]).getTime()) /
+    (1000 * 60 * 60 * 24)
+  );
+  const velocity =
+    sprintResult["totalStoryPointsCompleted"] / sprintDuration;
+
+  let logOutput = `Sprint Name: ${sprintResult["name"]}\n`;
+  logOutput += `Sprint Duration: ${sprintDuration} days\n`;
+  logOutput += `Sprint Start Date: ${sprintResult["startDate"]}\n`;
+  logOutput += `Sprint End Date: ${sprintResult["endDate"]}\n`;
+  logOutput += `Sprint Goal: ${sprintResult["goal"]}\n`;
+  logOutput += `Total Story Points Planned: ${sprintResult["totalStoryPointsPlanned"]}\n`;
+  logOutput += `Total Story Points Completed: ${sprintResult["totalStoryPointsCompleted"]}\n`;
+  logOutput += `Percentage of Planned Story Points Completed: ${sprintResult["percentageOfPlannedStoryPointsCompleted"]}%\n`;
+  logOutput += `Velocity: ${velocity.toFixed(2)} story points per day\n`;
+
+  logOutput += `\nUser Stories Completed:\n`;
+
+  for (const userStory of sprintResult["userStoriesCompleted"]) {
+    logOutput += `- ${userStory}\n`;
+  }
+
+  logOutput += `\nUser Stories Not Completed:\n`;
+  for (const userStory of sprintResult["userStoriesNotCompleted"]) {
+    logOutput += `- ${userStory}\n`;
+  }
+
+  logOutput += `\nImpediments:\n`;
+  for (const impediment of sprintResult["impediments"]) {
+    logOutput += `- ${impediment}\n`;
+  }
+
+  logOutput += `\nRetrospective Actions:\n`;
+  for (const action of sprintResult["retrospectiveActions"]) {
+    logOutput += `- ${action}\n`;
+  }
+
+  return logOutput;
+}
+
 export class PDFSprintReport extends Report {
   public author: User;
   public timeOfGeneration: string;
@@ -66,48 +97,7 @@ export class PDFSprintReport extends Report {
     return `This is the footer of the report that's going to be generated in PDF format`;
   }
 
-  protected generateContent(): string {
-    const sprintDuration = Math.round(
-      (new Date(this.sprintResult["endDate"]).getTime() -
-        new Date(this.sprintResult["startDate"]).getTime()) /
-        (1000 * 60 * 60 * 24)
-    );
-    const velocity =
-      this.sprintResult["totalStoryPointsCompleted"] / sprintDuration;
-
-    let logOutput = `Sprint Name: ${this.sprintResult["name"]}\n`;
-    logOutput += `Sprint Duration: ${sprintDuration} days\n`;
-    logOutput += `Sprint Start Date: ${this.sprintResult["startDate"]}\n`;
-    logOutput += `Sprint End Date: ${this.sprintResult["endDate"]}\n`;
-    logOutput += `Sprint Goal: ${this.sprintResult["goal"]}\n`;
-    logOutput += `Total Story Points Planned: ${this.sprintResult["totalStoryPointsPlanned"]}\n`;
-    logOutput += `Total Story Points Completed: ${this.sprintResult["totalStoryPointsCompleted"]}\n`;
-    logOutput += `Percentage of Planned Story Points Completed: ${this.sprintResult["percentageOfPlannedStoryPointsCompleted"]}%\n`;
-    logOutput += `Velocity: ${velocity.toFixed(2)} story points per day\n`;
-
-    logOutput += `\nUser Stories Completed:\n`;
-
-    for (const userStory of this.sprintResult["userStoriesCompleted"]) {
-      logOutput += `- ${userStory}\n`;
-    }
-
-    logOutput += `\nUser Stories Not Completed:\n`;
-    for (const userStory of this.sprintResult["userStoriesNotCompleted"]) {
-      logOutput += `- ${userStory}\n`;
-    }
-
-    logOutput += `\nImpediments:\n`;
-    for (const impediment of this.sprintResult["impediments"]) {
-      logOutput += `- ${impediment}\n`;
-    }
-
-    logOutput += `\nRetrospective Actions:\n`;
-    for (const action of this.sprintResult["retrospectiveActions"]) {
-      logOutput += `- ${action}\n`;
-    }
-
-    return logOutput;
-  }
+  protected generateContent(): string { return exportLog(this.sprintResult) }
 }
 
 export class PNGSprintReport extends Report {
@@ -130,48 +120,7 @@ export class PNGSprintReport extends Report {
     return `This is the footer of the report that's going to be generated in PNG format`;
   }
 
-  protected generateContent(): string {
-    const sprintDuration = Math.round(
-      (new Date(this.sprintResult["endDate"]).getTime() -
-        new Date(this.sprintResult["startDate"]).getTime()) /
-        (1000 * 60 * 60 * 24)
-    );
-    const velocity =
-      this.sprintResult["totalStoryPointsCompleted"] / sprintDuration;
-
-    let logOutput = `Sprint Name: ${this.sprintResult["name"]}\n`;
-    logOutput += `Sprint Duration: ${sprintDuration} days\n`;
-    logOutput += `Sprint Start Date: ${this.sprintResult["startDate"]}\n`;
-    logOutput += `Sprint End Date: ${this.sprintResult["endDate"]}\n`;
-    logOutput += `Sprint Goal: ${this.sprintResult["goal"]}\n`;
-    logOutput += `Total Story Points Planned: ${this.sprintResult["totalStoryPointsPlanned"]}\n`;
-    logOutput += `Total Story Points Completed: ${this.sprintResult["totalStoryPointsCompleted"]}\n`;
-    logOutput += `Percentage of Planned Story Points Completed: ${this.sprintResult["percentageOfPlannedStoryPointsCompleted"]}%\n`;
-    logOutput += `Velocity: ${velocity.toFixed(2)} story points per day\n`;
-
-    logOutput += `\nUser Stories Completed:\n`;
-
-    for (const userStory of this.sprintResult["userStoriesCompleted"]) {
-      logOutput += `- ${userStory}\n`;
-    }
-
-    logOutput += `\nUser Stories Not Completed:\n`;
-    for (const userStory of this.sprintResult["userStoriesNotCompleted"]) {
-      logOutput += `- ${userStory}\n`;
-    }
-
-    logOutput += `\nImpediments:\n`;
-    for (const impediment of this.sprintResult["impediments"]) {
-      logOutput += `- ${impediment}\n`;
-    }
-
-    logOutput += `\nRetrospective Actions:\n`;
-    for (const action of this.sprintResult["retrospectiveActions"]) {
-      logOutput += `- ${action}\n`;
-    }
-
-    return logOutput;
-  }
+  protected generateContent(): string { return exportLog(this.sprintResult) }
 }
 
 export class JPEGSprintReport extends Report {
@@ -194,46 +143,5 @@ export class JPEGSprintReport extends Report {
     return `This is the footer of the report that's going to be generated in JPEG format`;
   }
 
-  protected generateContent(): string {
-    const sprintDuration = Math.round(
-      (new Date(this.sprintResult["endDate"]).getTime() -
-        new Date(this.sprintResult["startDate"]).getTime()) /
-        (1000 * 60 * 60 * 24)
-    );
-    const velocity =
-      this.sprintResult["totalStoryPointsCompleted"] / sprintDuration;
-
-    let logOutput = `Sprint Name: ${this.sprintResult["name"]}\n`;
-    logOutput += `Sprint Duration: ${sprintDuration} days\n`;
-    logOutput += `Sprint Start Date: ${this.sprintResult["startDate"]}\n`;
-    logOutput += `Sprint End Date: ${this.sprintResult["endDate"]}\n`;
-    logOutput += `Sprint Goal: ${this.sprintResult["goal"]}\n`;
-    logOutput += `Total Story Points Planned: ${this.sprintResult["totalStoryPointsPlanned"]}\n`;
-    logOutput += `Total Story Points Completed: ${this.sprintResult["totalStoryPointsCompleted"]}\n`;
-    logOutput += `Percentage of Planned Story Points Completed: ${this.sprintResult["percentageOfPlannedStoryPointsCompleted"]}%\n`;
-    logOutput += `Velocity: ${velocity.toFixed(2)} story points per day\n`;
-
-    logOutput += `\nUser Stories Completed:\n`;
-
-    for (const userStory of this.sprintResult["userStoriesCompleted"]) {
-      logOutput += `- ${userStory}\n`;
-    }
-
-    logOutput += `\nUser Stories Not Completed:\n`;
-    for (const userStory of this.sprintResult["userStoriesNotCompleted"]) {
-      logOutput += `- ${userStory}\n`;
-    }
-
-    logOutput += `\nImpediments:\n`;
-    for (const impediment of this.sprintResult["impediments"]) {
-      logOutput += `- ${impediment}\n`;
-    }
-
-    logOutput += `\nRetrospective Actions:\n`;
-    for (const action of this.sprintResult["retrospectiveActions"]) {
-      logOutput += `- ${action}\n`;
-    }
-
-    return logOutput;
-  }
+  protected generateContent(): string { return exportLog(this.sprintResult) }
 }
